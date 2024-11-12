b89c6545514b0a42ac9f537401efc7a30d3bd88a
Ulrik Andersen <ulrik@shape.dk>
2024-11-12 15:16:32 +0100

    Use kotlin serialization in ktor end2end

--- end2end-tests/ktor/src/test/kotlin/com/cjbooms/fabrikt/servers/ktor/KtorGitHubApiServerTest.kt
+++ end2end-tests/ktor/src/test/kotlin/com/cjbooms/fabrikt/servers/ktor/KtorGitHubApiServerTest.kt
@@ -4,17 +4,13 @@ import com.example.controllers.ContributorsController.Companion.contributorsRout
 import com.example.controllers.InternalEventsController.Companion.internalEventsRoutes
 import com.example.models.BulkEntityDetails
 import com.example.models.EntityDetails
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
-import io.ktor.serialization.jackson.jackson
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.plugins.BadRequestException
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.plugins.statuspages.StatusPages
@@ -22,6 +18,24 @@ import io.ktor.server.response.respond
 import io.ktor.server.testing.ApplicationTestBuilder
 import io.ktor.server.testing.testApplication
 import io.mockk.slot
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.Json.Default.serializersModule
+import kotlinx.serialization.json.JsonDecoder
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.boolean
+import kotlinx.serialization.json.booleanOrNull
+import kotlinx.serialization.json.double
+import kotlinx.serialization.json.doubleOrNull
+import kotlinx.serialization.json.int
+import kotlinx.serialization.json.intOrNull
+import kotlinx.serialization.modules.SerializersModule
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -46,14 +60,8 @@ class KtorGitHubApiServerTest {
                     """
                     {
                         "entities": [{
-                            "id": "1",
-                            "properties": {
-                                "entity1PropKey": "entity1PropValue"
-                            }
-                        }],
-                        "properties": {
-                            "propKey": "propValue"
-                        }
+                            "id": "1"
+                        }]
                     }
                     """.trimIndent()
                 )
@@ -62,9 +70,11 @@ class KtorGitHubApiServerTest {
             assertEquals(
                 BulkEntityDetails(
                     entities = listOf(
-                        EntityDetails(id = "1", properties = mutableMapOf("entity1PropKey" to "entity1PropValue"))
+                        EntityDetails(id = "1",
+//                            properties = mutableMapOf("entity1PropKey" to "entity1PropValue")
+                        )
                     ),
-                    properties = mutableMapOf("propKey" to "propValue")
+//                    properties = mutableMapOf("propKey" to "propValue")
                 ),
                 bodyCapturingSlot.captured
             )
@@ -87,8 +97,7 @@ class KtorGitHubApiServerTest {
                 setBody(
                     """
                     {
-                        "entities": [],
-                        "properties": {}
+                        "entities": []
                     }
                     """.trimIndent()
                 )
@@ -218,13 +227,47 @@ class KtorGitHubApiServerTest {
         }
     }
 
+    object AnySerializer : KSerializer<Any> {
+        override val descriptor: SerialDescriptor = buildClassSerialDescriptor("Any")
+
+        override fun serialize(encoder: Encoder, value: Any) {
+            when (value) {
+                is Int -> encoder.encodeInt(value)
+                is String -> encoder.encodeString(value)
+                is Boolean -> encoder.encodeBoolean(value)
+                is Double -> encoder.encodeDouble(value)
+//                is DataTypeA -> encoder.encodeSerializableValue(DataTypeA.serializer(), value)
+                else -> throw SerializationException("Unsupported type: ${value::class}")
+            }
+        }
+
+        override fun deserialize(decoder: Decoder): Any {
+            val input = decoder as? JsonDecoder ?: throw SerializationException("This class can be loaded only by JSON")
+            val jsonElement = input.decodeJsonElement()
+            return when (jsonElement) {
+                is JsonPrimitive -> when {
+                    jsonElement.isString -> jsonElement.content
+                    jsonElement.booleanOrNull != null -> jsonElement.boolean
+                    jsonElement.intOrNull != null -> jsonElement.int
+                    jsonElement.doubleOrNull != null -> jsonElement.double
+                    else -> throw SerializationException("Unsupported primitive type: $jsonElement")
+                }
+                is JsonObject -> jsonElement
+                else -> throw SerializationException("Unsupported JSON element: $jsonElement")
+            }
+        }
+    }
+
     private fun ApplicationTestBuilder.configure() {
         install(ContentNegotiation) {
-            jackson()
+            json(Json {
+                serializersModule = SerializersModule { contextual(Any::class, AnySerializer) }
+            })
         }
 
         install(StatusPages) {
             exception<BadRequestException> { call, cause ->
+                println("Caught exception: ${cause.cause?.cause}")
                 call.respond(HttpStatusCode.BadRequest, cause.message ?: "Bad Request")
             }
         }
