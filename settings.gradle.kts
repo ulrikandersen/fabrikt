57fbe7a0b713ada9fc4943d4d1326a75e27771f1
Ulrik Andersen <ulrik@shape.dk>
2025-01-06 14:23:37 +0100

    Rename ktor test to ktor-jackson
    Prepares for similar end2end test for Kotlin serialization

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "fabrikt"
 include(
     "end2end-tests:okhttp",
     "end2end-tests:openfeign",
-    "end2end-tests:ktor",
+    "end2end-tests:ktor-jackson",
     "end2end-tests:models-jackson",
     "end2end-tests:models-kotlinx",
 )
